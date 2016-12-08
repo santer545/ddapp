4e89373a4c61214f7c4ffd077d7c2dd3efe3be5c
santer545 <obram22@gmail.com>
2016-12-08 18:39:55 +0200

    diagramm & orders

--- js/main.js
+++ js/main.js
@@ -15,4 +15,13 @@ $(document).ready(function() {
     $('.js-edit').click(function() {
         $('.js-editing input, .js-editing button, .js-editing textarea').removeAttr('disabled');
     });
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ]
+        }
+    });
 });
