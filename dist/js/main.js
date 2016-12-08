b5391c9b3d4f2362a6454cf5e2ad70c341b078c9
Obramenko <obram22@gmail.com>
2016-12-08 21:34:58 +0200

    fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,8 +19,7 @@ $(document).ready(function() {
         bindto: '#chart',
         data: {
             columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
+                ['data1', 500, 1000, 1500, 2000],
             ]
         }
     });
