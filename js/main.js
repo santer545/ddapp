aca8b1845d36983b1ab0cfdeeb55197de5fd3603
santer545 <obram22@gmail.com>
2016-12-09 17:52:44 +0200

    create order

--- js/main.js
+++ js/main.js
@@ -15,12 +15,12 @@ $(document).ready(function() {
     $('.js-edit').click(function() {
         $('.js-editing input, .js-editing button, .js-editing textarea').removeAttr('disabled');
     });
-    var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-            columns: [
-                ['data1', 500, 1000, 1500, 2000],
-            ]
-        }
+    $(document).ready(function(){
+      $(".owl-carousel").owlCarousel({
+        items: 1,
+        mouseDrag: false,
+        nav: true,
+        navText: ['<svg class="icon icon-ico35"><use xlink:href="#icon-ico35"></use></svg> предыдущая неделя', 'следующая неделя <svg class="icon icon-ico36"><use xlink:href="#icon-ico36"></use></svg>']
+      });
     });
 });
