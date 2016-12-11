a9c48ed97ef8c5fe4716789778b06c9ebebe3155
Obramenko <obram22@gmail.com>
2016-12-11 22:16:17 +0200

    add product

--- js/main.js
+++ js/main.js
@@ -15,12 +15,39 @@ $(document).ready(function() {
     $('.js-edit').click(function() {
         $('.js-editing input, .js-editing button, .js-editing textarea').removeAttr('disabled');
     });
-    $(document).ready(function(){
-      $(".owl-carousel").owlCarousel({
-        items: 1,
-        mouseDrag: false,
-        nav: true,
-        navText: ['<svg class="icon icon-ico35"><use xlink:href="#icon-ico35"></use></svg> предыдущая неделя', 'следующая неделя <svg class="icon icon-ico36"><use xlink:href="#icon-ico36"></use></svg>']
-      });
+    $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            mouseDrag: false,
+            nav: true,
+            navText: ['<svg class="icon icon-ico35"><use xlink:href="#icon-ico35"></use></svg> предыдущая неделя', 'следующая неделя <svg class="icon icon-ico36"><use xlink:href="#icon-ico36"></use></svg>']
+        });
+    });
+
+
+
+    /**
+    Product preview
+    **/
+    function readURL(input) {
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#img__preview').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#file").change(function() {
+        readURL(this);
+    });
+
+    $('#product-name').on('changed.bs.select', function(e) {
+        var prod_name = $('[data-id="product-name"] .filter-option').text();
+        console.log(prod_name);
+        $('.add__name').html(prod_name);
     });
 });
