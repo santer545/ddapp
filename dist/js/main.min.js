3af952d19c11fdb8c4fcd62ce137004d71dbe500
Obramenko <obram22@gmail.com>
2016-12-25 22:17:03 +0200

    add product

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
         $(".sidebar").toggleClass("active");
         $(this).toggleClass("active");
     });
+    $(".load__small").click(function() {
+        $(this).toggleClass("active");
+    });
     // Bootstrap select
     $('.selectpicker').selectpicker({
         dropupAuto: false
@@ -23,7 +26,128 @@ $(document).ready(function() {
         navText: ['<svg class="icon icon-ico35"><use xlink:href="#icon-ico35"></use></svg> <span>предыдущая неделя</a>', '<span>следующая неделя</span> <svg class="icon icon-ico36"><use xlink:href="#icon-ico36"></use></svg>']
     });
 
-    var pattern = $('.add__product_wrapper').find('.add__product:first-child').clone().wrap('<div></div>').parent().html();
+    var pattern = `main.js:105 <div class="add__product">
+                                <div class="row">
+                                    <div class="col-md-8 col-sm-8 col-xs-8">
+                                        <p class="add__heading">Заполните данные</p>
+                                        <div class="add__item">
+                                            <input type="file" name="file" id="file" class="inputfile">
+                                            <div class="load__small js-color">
+                                                <svg class="icon icon-plus icon__gray">
+                                                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-plus"></use>
+                                                </svg>
+                                            </div>
+                                            <label for="file" class="load__img">
+                                                <div class="load__holder">
+                                                    <svg class="icon icon-plus icon__gray">
+                                                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-plus"></use>
+                                                    </svg>
+                                                    <span>Загрузить фото</span>
+                                                </div>
+                                            </label>
+                                        </div>
+                                        <div class="add__brand">
+                                            <div class="row">
+                                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                                    <label for="product-name" class="control-label control-label--big">Брендлайн</label>
+                                                </div>
+                                                <div class="col-md-9 col-sm-9 col-xs-9">
+                                                    <div class="btn-group bootstrap-select"><button type="button" class="btn dropdown-toggle bs-placeholder btn-default" data-toggle="dropdown" role="button" data-id="product-name" title="Модель 213 рисунок 1220, голубой"><span class="filter-option pull-left">Модель 213 рисунок 1220, голубой</span>&nbsp;<span class="bs-caret"><span class="caret"></span></span></button><div class="dropdown-menu open" role="combobox"><ul class="dropdown-menu inner" role="listbox" aria-expanded="false"><li data-original-index="0" class="selected"><a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="true"><span class="text">Модель 213 рисунок 1220, голубой</span><span class="glyphicon glyphicon-ok check-mark"></span></a></li><li data-original-index="1"><a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false"><span class="text">Модель 213 рисунок 20, неголубой</span><span class="glyphicon glyphicon-ok check-mark"></span></a></li><li data-original-index="2"><a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false"><span class="text">Модель 213 рисунок 0, гоголубой</span><span class="glyphicon glyphicon-ok check-mark"></span></a></li><li data-original-index="3"><a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false"><span class="text">Модель 213 рисунок 1, неголубой</span><span class="glyphicon glyphicon-ok check-mark"></span></a></li></ul></div><select name="" id="product-name" class="selectpicker" tabindex="-98">
+                                                        <option value="">Модель 213 рисунок 1220, голубой</option>
+                                                        <option value="">Модель 213 рисунок 20, неголубой</option>
+                                                        <option value="">Модель 213 рисунок 0, гоголубой</option>
+                                                        <option value="">Модель 213 рисунок 1, неголубой</option>
+                                                    </select></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="add__info">
+                                            <div class="add__tr">
+                                                <div class="add__th add__size_block add__size_blcok--th">Размер</div>
+                                                <div class="add__th add__price_category add__price_category--th">Ценовая категория</div>
+                                                <div class="add__th add__price add__price--th">Ф1</div>
+                                                <div class="add__th add__price add__price--th">Ф2</div>
+                                                <div class="add__th add__plus"></div>
+                                                <div class="add__th">
+                                                    <div class="add__delete js_delete_tr">
+                                                        <svg class="icon icon-ico33 icon__gray">
+                                                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-ico33"></use>
+                                                        </svg>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="add__tr add__tr--dynamic">
+                                                <div class="add__td add__size_block add__size_block--td">
+                                                <input type="text" class="form-control form-control--size" value="21-23">
+                                                </div>
+                                                <div class="add__td add__price_category add__price_category--td">
+                                                    <div class="btn-group bootstrap-select"><button type="button" class="btn dropdown-toggle bs-placeholder btn-default" data-toggle="dropdown" role="button" data-id="" title="минимаркет"><span class="filter-option pull-left">минимаркет</span>&nbsp;<span class="bs-caret"><span class="caret"></span></span></button><div class="dropdown-menu open" role="combobox"><ul class="dropdown-menu inner" role="listbox" aria-expanded="false"><li data-original-index="0" class="selected"><a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="true"><span class="text">минимаркет</span><span class="glyphicon glyphicon-ok check-mark"></span></a></li><li data-original-index="1"><a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false"><span class="text">HoReca</span><span class="glyphicon glyphicon-ok check-mark"></span></a></li></ul></div><select name="" class="selectpicker" tabindex="-98">
+                                                        <option value="">минимаркет</option>
+                                                        <option value="">HoReca</option>
+                                                    </select></div>
+                                                </div>
+                                                <div class="add__td add__price add__price--td">
+                                                    <input type="text" class="form-control--table form-control" value="6 112, 45">
+                                                </div>
+                                                <div class="add__td add__price add__price--td">
+                                                    <input type="text" class="form-control--table form-control" value="6 112, 45">
+                                                </div>
+                                                <div class="add__plus add__td">
+                                                    <div class="add__holder add__holder--big js__add_row">
+                                                        <svg class="icon icon-plus">
+                                                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-plus"></use>
+                                                        </svg>
+                                                    </div>
+                                                </div>
+                                                <div class="add__td">
+                                                    <div class="add__delete js_delete_tr">
+                                                        <svg class="icon icon-ico33 icon__gray">
+                                                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-ico33"></use>
+                                                        </svg>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="add__size text-center js_add_size">
+                                            добавить размер
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <p class="add__heading">Предпросмотр</p>
+                                        <div class="add__view">
+                                            <ul class="f__list">
+                                                <li id="id_f1" class="f__item active">Ф1</li>
+                                                <li id="id_f2" class="f__item">Ф2</li>
+                                            </ul>
+                                            <ul class="color__list">
+                                                <li class="color__item active js-color"></li>
+                                            </ul>
+                                            <figure>
+                                                <img id="img__preview" src="" alt="">
+                                            </figure>
+                                        </div>
+                                        <div class="add__name">
+                                            Модель 321 - рисунок 1220, голубой
+                                        </div>
+                                        <div class="add__info--view">
+                                            <table class="add__table">
+                                                <tbody><tr>
+                                                    <th class="add__table_th">Размер</th>
+                                                    <th class="add__table_th">Кол-во</th>
+                                                    <th class="add__table_th">Цена</th>
+                                                </tr>
+                                                <tr>
+                                                    <td class="add__table_td">21-23</td>
+                                                    <td class="add__table_td"><span class="preview__number bordered">0</span></td>
+                                                    <td class="add__table_td">12,35</td>
+                                                </tr>
+                                            </tbody></table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+    /*var pattern = $('.add__product_wrapper').find('.add__product:first-child').clone().wrap('<div></div>').parent().html();*/
 
     /**
     Product preview
@@ -104,13 +228,14 @@ $(document).ready(function() {
     });
 
 
-    $('.add__product_wrapper').on('click', '.js_delete_tr', function() {
-        var row = $(this).closest('.add__tr--dynamic').html();
+    $('.add__info').on('click', '.js_delete_tr', function() {
         $(this).closest('.add__tr--dynamic').remove();
+        console.log(this);
     });
-    $('.add__product_wrapper').on('click', '.js__add_row', function() {
-        var row = $(this).closest('.add__tr').html();
+    $('.add__info').on('click', '.js__add_row', function() {
+        var row = $('.js__add_row').closest('.add__tr--dynamic').clone().wrap('<div></div>').parent().html();
         $(this).closest('.add__tr').after(row);
+
     });
     $('.add__product_wrapper').on('click', '.js_add_size', function() {
         var info = $(this).siblings('.add__info:eq(0)').clone();
@@ -431,4 +556,16 @@ $('#check-2').on('change', function() {
             $(this).removeClass('ui-selected');
         });
     }
+});
+
+
+
+
+// initialize on multiple elements with jQuery
+$('.js-color').each(function(i, elem) {
+    var hueb = new Huebee(elem, {
+        // options
+        setText: false,
+        setBGColor: '.color__item'
+    });
 });
